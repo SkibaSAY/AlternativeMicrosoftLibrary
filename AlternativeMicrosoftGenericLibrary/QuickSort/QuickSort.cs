06e718d00b4b8e1842aa1d06453e0f63145ca929
Anton Slastuhin <antonslastuhin@gmail.com>
2023-06-21 15:02:10 +0300

    Написал быструю сортировку через рекурсию Написал тесты для сортировки, но почему-то не проходят. Зацикливаемся при вызовах

---
+++ AlternativeMicrosoftGenericLibrary/QuickSort/QuickSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternativeMicrosoftGenericLibrary.QuickSort
+{
+    public static class QuickSortExtention<T>
+        where T:IComparable<T>
+    {
+        public static void QuickSort(T[] arr,int l = -1,int r = -1,int statementIndex = 0)
+        {
+            //инициализация            
+            if (l == -1) l = 0;
+            if (r == -1) r = arr.Length - 1;
+            if (r - l + 1 < 2) return;
+
+            var initL = l;
+            var initR = r;
+
+            var statementElement = arr[statementIndex];
+
+            while(l <= r)
+            {
+                while(l <= r && arr[l].CompareTo(statementElement) < 0)
+                {
+                    l++;
+                }
+                while (l <= r && arr[r].CompareTo(statementElement) > 0)
+                {
+                    r--;
+                }
+                
+                //меняем элементы местами
+                if(l <= r)
+                {
+                    var temp = arr[l];
+                    arr[l] = arr[r];
+                    arr[r] = temp;
+                    l++;
+                    r--;
+                }
+            }
+            if (r > initL)QuickSort(arr, l: initL, r: r);
+            if (l < initR) QuickSort(arr, l: l, r: initR);
+        }
+    }
+}
---
+++ UnitTests/QuickSortExtention/QuickSort.cs
@@ -0,0 +1,45 @@
+﻿using AlternativeMicrosoftGenericLibrary.QuickSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace QuickSortExtention
+{
+    [TestClass]
+    public class QuickSort
+    {
+        [TestMethod]
+        public void QuickSortRandom()
+        {
+            var random = new Random();
+            var n = 10;
+            var arr = new int[n];
+
+            for(var i = 0; i < n; i++)
+            {
+                arr[i] = random.Next();
+            }
+
+            var arrByQuickSort = new int[n];
+            arr.CopyTo(arrByQuickSort,0);
+
+            Array.Sort(arr);
+            QuickSortExtention<int>.QuickSort(arrByQuickSort);
+
+            for(var i = 0; i < n; i++)
+            {
+                Assert.AreEqual(arr[i], arrByQuickSort[i]);
+            }
+        }
+
+        [TestMethod]
+        public void QuickSortSuperRandom()
+        {
+            var m = 100;
+            while(m > 0)
+            {
+                QuickSortRandom();
+                m--;
+            }
+        }
+    }
+}
